9ff3c63fa13124ea5d73033d0a4440e091a3c4d9
Czar <127335392+Czar81@users.noreply.github.com>
2025-02-27 09:57:48 -0600

    Update bank.py

--- M1/S12/pIllars_oop/bank.py
+++ M1/S12/pIllars_oop/bank.py
@@ -4,42 +4,43 @@ class BankAccount():
     def add_money(self, money):
         self.balance += money
         print("Money was add succesful")
-    
+
+
+class SavingsAccount(BankAccount):
+    def __init__(self, min_balance):
+        self.min_balance = min_balance
+
 
     def withdraw(self, money):
-        if (self.balance - money) > self.min_balance:
+        if (self.balance - money) >= self.min_balance:
             self.balance -= money
             print("Withdraw succesful")
         else:
             print("Can not do the withdraw, balance under minimun balance ")
 
 
-class SavingsAccount(BankAccount):
-    def __init__(self, min_balance):
-        self.min_balance = min_balance
-        pass
-
-
 def __input_amount(text):
-    try:
-        amount = int(input(text))
-        if amount < 0:
-            raise ValueError
-        else:
-            return amount
-    except ValueError as error:
-        print(f"Error, invalid value, must be a positive number: {error}")
+    while True:
+        try:
+            amount = int(input(text))
+            if amount < 0:
+                raise ValueError
+            else:
+                return amount
+        except ValueError as error:
+            print(f"Error, invalid value, must be a positive number: {error}")
 
 
 def __input_menu():
-    try:
-        option = int(input("Enter an option: "))
-        if option > 3  or option < 1:
-            raise ValueError
-        else:
-            return option
-    except ValueError as error:
-        print(f"Error, value must be between 1 and 3: {error}")
+    while True:
+        try:
+            option = int(input("Enter an option: "))
+            if option > 3  or option < 1:
+                raise ValueError
+            else:
+                return option
+        except ValueError as error:
+            print(f"Error, value must be between 1 and 3: {error}")
 
 
 def __menu():
