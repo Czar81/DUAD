79a48b39d1da5abcbc718a525f3cb8fee041d218
Czar <ianaaronza@gmail.com>
2025-03-19 15:30:39 -0600

    New Revenues interface working

---
+++ M1/S17/personal_finance_management/gui/interface_new_revenues.py
@@ -0,0 +1,25 @@
+import FreeSimpleGUI as fsg
+
+def _make_new_revenues_interface():
+    
+    # ------ Window Layout ------
+    layout = [
+        [fsg.Text(text="Enter Title")],
+        [fsg.Input()], 
+        [fsg.Text(text="Enter Amount")],
+        [fsg.Input()], 
+        [fsg.Text(text="Enter Category")],
+        [fsg.Input()], 
+        [fsg.Button(button_text="Create")]
+    ]
+
+    # ------ Create Window ------
+    window = fsg.Window(title="New Revenues", layout=layout)
+
+    # ------ Event Loop ------
+    while True:
+        event, values = window.read()
+        if event is None:
+            break  
+
+    window.close()
