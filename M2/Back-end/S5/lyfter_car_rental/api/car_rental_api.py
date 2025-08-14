70a822ca26b7910298d26f84cd745b63b5eadf9d
Czar <ianaaronza@gmail.com>
2025-08-14 17:30:36 -0600

    Add Value error and str() to exceptions

--- M2/Back-end/S5/lyfter_car_rental/api/car_rental_api.py
+++ M2/Back-end/S5/lyfter_car_rental/api/car_rental_api.py
@@ -13,18 +13,20 @@ class CarRentalView(MethodView):
             request_body = request.json
             response = self.entity_repo.create(request_body)
             return jsonify({"message":"Created successfully"}), response
+        except ValueError as e:
+            return jsonify({"message":str(e)}), 409
         except KeyError as e:
-            return jsonify({"message":f"Request key does not exist: {e}"}), 400
+            return jsonify({"message":f"Request key does not exist: {str(e)}"}), 400
         except IntegrityError:
-            return jsonify({"message":f"Username or email already exists"}), 409
+            return jsonify({"message":f"Some data already exists"}), 409
         except DataError as e:
-            return jsonify({"message":f"Invalid data format: {e}"}), 422 
+            return jsonify({"message":f"Invalid data format: {str(e)}"}), 422 
         except OperationalError as e:
-            return jsonify({"message":f"Database operation failed: {e}"}) , 503      
+            return jsonify({"message":f"Database operation failed: {str(e)}"}) , 503      
         except InterfaceError as e:
-            return jsonify({"message":f"Error trying to communicate with db: {e}"}), 500
+            return jsonify({"message":f"Error trying to communicate with db: {str(e)}"}), 500
         except Exception as e:
-            return jsonify({"message":f"Unexpected error occurred: {e}"}), 500
+            return jsonify({"message":f"Unexpected error occurred: {str(e)}"}), 500
     
     def put(self, id):
         try:
@@ -39,15 +41,15 @@ class CarRentalView(MethodView):
                 response = self.entity_repo.change_state(id, new_state)
             return jsonify({"message": "State changed successfully"}), response
         except KeyError as e:
-            return jsonify({"message":f"State key does not exist: {e}"}), 400
+            return jsonify({"message":f"State key does not exist: {str(e)}"}), 400
         except DataError as e:
-            return jsonify({"message":f"Invalid data format: {e}"}), 422 
+            return jsonify({"message":f"Invalid data format: {str(e)}"}), 422 
         except OperationalError as e:
-            return jsonify({"message":f"Database operation failed: {e}"}) , 503      
+            return jsonify({"message":f"Database operation failed: {str(e)}"}) , 503      
         except InterfaceError as e:
-            return jsonify({"message":f"Error trying to communicate with db: {e}"}), 500
+            return jsonify({"message":f"Error trying to communicate with db: {str(e)}"}), 500
         except Exception as e:
-            return jsonify({"message":f"Unexpected error occurred: {e}"}), 500
+            return jsonify({"message":f"Unexpected error occurred: {str(e)}"}), 500
     
     def get(self):
         try:
@@ -58,13 +60,13 @@ class CarRentalView(MethodView):
                 results, response = self.entity_repo.get_all()
             return jsonify({"return":results}), response
         except KeyError as e:
-            return jsonify({"message":f"Request keys do not exist: {e}"}), 400
+            return jsonify({"message":f"Request keys do not exist: {str(e)}"}), 400
         except DataError as e:
-            return jsonify({"message":f"Invalid data format: {e}"}), 422 
+            return jsonify({"message":f"Invalid data format: {str(e)}"}), 422 
         except OperationalError as e:
-            return jsonify({"message":f"Database operation failed: {e}"}) , 503      
+            return jsonify({"message":f"Database operation failed: {str(e)}"}) , 503      
         except InterfaceError as e:
-            return jsonify({"message":f"Error trying to communicate with db: {e}"}), 500
+            return jsonify({"message":f"Error trying to communicate with db: {str(e)}"}), 500
         except Exception as e:
-            return jsonify({"message":f"Unexpected error occurred: {e}"}), 500
+            return jsonify({"message":f"Unexpected error occurred: {str(e)}"}), 500
         
