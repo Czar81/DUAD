cfa7972c09c0488197224f4d27344407c74115bc
Czar <ianaaronza@gmail.com>
2025-08-14 17:00:29 -0600

    Create just one MethodView for 3 identities

--- M2/Back-end/S5/lyfter_car_rental/api/car_rental_api.py
+++ M2/Back-end/S5/lyfter_car_rental/api/car_rental_api.py
@@ -3,22 +3,16 @@ from flask.views import MethodView
 from psycopg2 import IntegrityError, DataError, OperationalError, InterfaceError
 
 
-class UserView(MethodView):
+class CarRentalView(MethodView):
 
-    def __init__(self, user_repo):
-        self.user_repo = user_repo
+    def __init__(self, entity_repo):
+        self.entity_repo = entity_repo
 
     def post(self):
         try:
             request_body = request.json
-            response = self.user_repo.create_user(
-                name=request_body["name"],
-                email=request_body["email"],
-                username=request_body["username"],
-                password=request_body["password"],
-                birthday=request_body["birthday"]
-            )
-            return jsonify({"message":"User created successfully"}), response
+            response = self.entity_repo.create(request_body)
+            return jsonify({"message":"Created successfully"}), response
         except KeyError as e:
             return jsonify({"message":f"Request key does not exist: {e}"}), 400
         except IntegrityError:
@@ -34,12 +28,16 @@ class UserView(MethodView):
     
     def put(self, id):
         try:
+            print(request.path)
             new_state = request.json['state']
             if new_state == "Debtor":
-                response=self.user_repo.put_user_debtor(id)
+                response=self.entity_repo.put_user_debtor(id)
+            elif new_state == "Return":
+                response=self.entity_repo.car_returned(id)
+                return jsonify({"message":"Car return successfully"}), response
             else:
-                response = self.user_repo.change_user_state(id, new_state)
-            return jsonify({"message": "User state changed successfully"}), response
+                response = self.entity_repo.change_state(id, new_state)
+            return jsonify({"message": "State changed successfully"}), response
         except KeyError as e:
             return jsonify({"message":f"State key does not exist: {e}"}), 400
         except DataError as e:
@@ -55,9 +53,9 @@ class UserView(MethodView):
         try:
             request_body = dict(request.args)
             if request_body != {}:
-                results, response = self.user_repo.get_users_by_filters(**request_body)
+                results, response = self.entity_repo.get_by_filters(**request_body)
             elif request_body == {}:
-                results, response = self.user_repo.get_all_users()
+                results, response = self.entity_repo.get_all()
             return jsonify({"return":results}), response
         except KeyError as e:
             return jsonify({"message":f"Request keys do not exist: {e}"}), 400
--- M2/Back-end/S5/lyfter_car_rental/main.py
+++ M2/Back-end/S5/lyfter_car_rental/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from api.user_api import UserView
-from api.car_api import CarView
+from api.car_rental_api import CarRentalView
 from scripts.database.user_repository import UserRepository
 from scripts.database.car_repository import CarRepository
 from scripts.database.rent_repository import RentRepository
@@ -14,8 +13,11 @@ user_repo = UserRepository(db_manager)
 car_repo = CarRepository(db_manager)
 rent_repo = RentRepository(db_manager)
 
-app.add_url_rule("/user", view_func=UserView.as_view("users",user_repo))
-app.add_url_rule("/user/<int:id>", view_func=UserView.as_view("user",user_repo))
-app.add_url_rule("/car", view_func=CarView.as_view("cars", car_repo))
-app.add_url_rule("/car/<int:id>", view_func=CarView.as_view("car", car_repo))
+app.add_url_rule("/user", view_func=CarRentalView.as_view("user",user_repo))
+app.add_url_rule("/user/<int:id>", view_func=CarRentalView.as_view("users",user_repo))
+app.add_url_rule("/car", view_func=CarRentalView.as_view("cars", car_repo))
+app.add_url_rule("/car/<int:id>", view_func=CarRentalView.as_view("car", car_repo))
+app.add_url_rule("/rent", view_func=CarRentalView.as_view("rents", rent_repo))
+app.add_url_rule("/rent/<int:id>", view_func=CarRentalView.as_view("rent", rent_repo))
+
 app.run(debug=True, host="localhost", port=5000)
