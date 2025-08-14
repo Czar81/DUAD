8ffff08df23405253b02c5ba9b3629ed5d5ca047
Czar <ianaaronza@gmail.com>
2025-08-14 16:57:17 -0600

    Add last get,get_by_filter and change state on rent_repository

--- M2/Back-end/S5/lyfter_car_rental/scripts/database/rent_repository.py
+++ M2/Back-end/S5/lyfter_car_rental/scripts/database/rent_repository.py
@@ -2,29 +2,38 @@ class RentRepository:
 
     def __init__(self, db_manager):
         self.db_manager = db_manager
+    
+    def _format_rent(self, rent_result):
+        return {
+            "id": rent_result[0],
+            "fk_car_id":rent_result[1],
+            "fk_user_id":rent_result[2],
+            "rent_date":rent_result[3],
+            "state":rent_result[4]
+        }
 
-    def create_rent(self, fk_car_id, fk_user_id, state):
+    def create(self, request_body):
         try:
+            
             self.db_manager.execute_query(
                 """
                 INSERT INTO lyfter_car_rental."Rents" (fk_car_id, fk_user_id, state)
-                VALUES(%s,%s,%s);
+                VALUES(%s,%s,'Active');
                 """,
-                fk_car_id,
-                fk_user_id,
-                state,
+                request_body["fk_car_id"],
+                request_body["fk_user_id"],
             )
             self.db_manager.execute_query(
                 """
                 UPDATE lyfter_car_rental."Cars" 
-                SET state = "Rented" 
+                SET state = 'Rented'
                 WHERE id = %s
                 """,
-                fk_car_id,
+                request_body["fk_car_id"],
             )
-
-        except Exception as e:
-            print(f"Error: {e}")
+            return 201
+        except Exception:
+            raise
 
     def car_returned(self, rent_id):
         try:
@@ -40,9 +49,43 @@ class RentRepository:
                 """
                 UPDATE lyfter_car_rental."Cars" 
                 SET state = 'Available' 
-                WHERE id = (SELECT id FROM lyfter_car_rental."Rents" WHERE id = %s); 
+                WHERE id = (SELECT fk_car_id FROM lyfter_car_rental."Rents" WHERE id = %s); 
                 """,
                 rent_id,
             )
-        except Exception as e:
-            print(f"Error: {e}")
+        except Exception:
+            raise
+    
+    def change_state(self, rent_id, new_state):
+        try:
+            self.db_manager.execute_query(
+                'UPDATE lyfter_car_rental."Rents" SET state = %s WHERE id = %s',
+                new_state,
+                rent_id
+            )
+            return 200
+        except Exception:
+            raise
+    
+    def get_all(self):
+        try:
+            results = self.db_manager.execute_query('SELECT * FROM lyfter_car_rental."Rents"')
+            formatted_results = list(map(self._format_rent, results))
+            return formatted_results, 200
+        except Exception:
+            raise
+    
+    def get_by_filters(self, **filters):
+        try:
+            base_query = 'SELECT * FROM lyfter_car_rental."Rents"'
+
+            where_query = [f'"{key}"=%s' for key in filters.keys()]
+            params = list(filters.values())
+
+            query = base_query + " WHERE " + " AND ".join(where_query)
+            results = self.db_manager.execute_query(query, *params)
+
+            formatted_results = list(map(self._format_rent, results))
+            return formatted_results, 200
+        except Exception:
+            raise
