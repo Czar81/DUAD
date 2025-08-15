a7ed97dd2f0f9563e0689d4229a47127f0012ed6
Czar <ianaaronza@gmail.com>
2025-08-15 06:51:58 -0600

    Add if __name__... and instruction to run both mains

--- M2/Back-end/S5/README.md
+++ M2/Back-end/S5/README.md
@@ -3,3 +3,5 @@
 ## Week 5
 
 This week is about Postgres and how to manage it with python
+
+To excecute the scripts run this [file](/M2/Back-end/S5/lyfter_car_rental/scripts/main.py), for run the API use this other [file](/M2/Back-end/S5/lyfter_car_rental/main.py)
--- M2/Back-end/S5/lyfter_car_rental/main.py
+++ M2/Back-end/S5/lyfter_car_rental/main.py
@@ -5,19 +5,20 @@ from scripts.database.car_repository import CarRepository
 from scripts.database.rent_repository import RentRepository
 from scripts.database.pg_manager import PgManager
 
-app = Flask(__name__)
-db_manager = PgManager(
-    db_name="postgres", user="postgres", password="postgres", host="localhost"
-)
-user_repo = UserRepository(db_manager)
-car_repo = CarRepository(db_manager)
-rent_repo = RentRepository(db_manager)
+if __name__ == "__main__":
+    app = Flask(__name__)
+    db_manager = PgManager(
+        db_name="postgres", user="postgres", password="Ian192007", host="localhost"
+    )
+    user_repo = UserRepository(db_manager)
+    car_repo = CarRepository(db_manager)
+    rent_repo = RentRepository(db_manager)
 
-app.add_url_rule("/user", view_func=CarRentalView.as_view("user",user_repo))
-app.add_url_rule("/user/<int:id>", view_func=CarRentalView.as_view("users",user_repo))
-app.add_url_rule("/car", view_func=CarRentalView.as_view("cars", car_repo))
-app.add_url_rule("/car/<int:id>", view_func=CarRentalView.as_view("car", car_repo))
-app.add_url_rule("/rent", view_func=CarRentalView.as_view("rents", rent_repo))
-app.add_url_rule("/rent/<int:id>", view_func=CarRentalView.as_view("rent", rent_repo))
+    app.add_url_rule("/user", view_func=CarRentalView.as_view("user",user_repo))
+    app.add_url_rule("/user/<int:id>", view_func=CarRentalView.as_view("users",user_repo))
+    app.add_url_rule("/car", view_func=CarRentalView.as_view("cars", car_repo))
+    app.add_url_rule("/car/<int:id>", view_func=CarRentalView.as_view("car", car_repo))
+    app.add_url_rule("/rent", view_func=CarRentalView.as_view("rents", rent_repo))
+    app.add_url_rule("/rent/<int:id>", view_func=CarRentalView.as_view("rent", rent_repo))
 
-app.run(debug=True, host="localhost", port=5000)
+    app.run(debug=True, host="localhost", port=5000)
