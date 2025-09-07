8f76eec05cf6d65ef114f315f0649e1161f51765
Czar <ianaaronza@gmail.com>
2025-09-07 12:06:07 -0600

    Create table manager with tables and Metada obj

---
+++ M2/Back-end/S7/Authentication/store/tables_metadata.py
@@ -0,0 +1,41 @@
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, DATE, func
+
+class TablesManager:
+    engine = create_engine('postgresql+psycopg2://postgres:Ian192007@localhost:5432/postgres')
+    metadata_obj = MetaData()
+    product_table = Table(
+        "products",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("name", String(30)),
+        Column("price", Integer()),
+        Column("entry_date", DATE, server_default=func.now()),
+        Column("amount", Integer()),
+    )
+    user_table = Table(
+        "users",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("username", String(30)),
+        Column("password", String),
+    )
+    receipt_table = Table(
+        "receipt",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("id_user", Integer, ForeignKey("users.id")),
+    )
+    receipt_details_table = Table(
+        "receipt_details",
+        metadata_obj,
+        Column("id", Integer, primary_key=True),
+        Column("id_receipt", Integer, ForeignKey("receipt.id")),
+        Column("id_product", Integer, ForeignKey("products.id")),
+        Column("amount", Integer),
+    )
+    @classmethod
+    def create_tables(cls):   
+        cls.metadata_obj.create_all(cls.engine)
+        
