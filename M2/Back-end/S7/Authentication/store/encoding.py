4714669114542979bb507d6d464e620cb83dee53
Czar <ianaaronza@gmail.com>
2025-09-07 17:50:52 -0600

    Add classmethod and add default secret and algorithm

--- M2/Back-end/S7/Authentication/store/encoding.py
+++ M2/Back-end/S7/Authentication/store/encoding.py
@@ -2,10 +2,11 @@ import jwt
 
 
 class JWT_Manager:
-    def __init__(self, secret, algorithm):
+    def __init__(self, secret="HelloThere", algorithm="HS256"):
         self.secret = secret
         self.algorithm = algorithm
 
+    @classmethod
     def encode(self, data):
         try:
             encoded = jwt.encode(data, self.secret, algorithm=self.algorithm)
@@ -13,6 +14,7 @@ class JWT_Manager:
         except:
             return None
 
+    @classmethod
     def decode(self, token):
         try:
             decoded = jwt.decode(token, self.secret, algorithms=[self.algorithm])
