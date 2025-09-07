5c0d253430cf66203aa7a78328e831aa734555a7
Czar <ianaaronza@gmail.com>
2025-09-06 19:14:29 -0600

    Get receipt by user

--- M2/Back-end/S7/Authentication/store/apis/receipt_api.py
+++ M2/Back-end/S7/Authentication/store/apis/receipt_api.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import jsonify, request
 from db.db_receipt_manager import DbReceiptManager
+from encoding import JWT_Manager
 
 app = Flask(__name__)
 db_receipt_manager = DbReceiptManager()
-
+jwt_manager=JWT_Manager("MyNameIsJeff","HS256")
 
 @app.route("/sales", methods=["POST"])
 def register_receipt():
@@ -16,3 +17,10 @@ def register_receipt():
         return None
     else:
         db_receipt_manager.create_receipt(id_user, id_product, amount)
+        return jsonify({"message":"Receipt created successfully"}),201
+
+@app.route("/sales/<token>/receipt", methods=["GET"])
+def get_user_receipt(token):
+    token_decoded = db_receipt_manager.decode(token)
+    receipts = db_receipt_manager.get_receipt_by_user_id(token_decoded)
+    return jsonify({"receipts":receipts}),200
