f81c9b6ac2e01d7673d5d3499ef9daf77e304890
Czar81 <ianaaronza@gmail.com>
2025-09-29 14:53:34 -0600

    Copy base project

---
+++ M2/Back-end/S8/store/apis/product_api.py
@@ -0,0 +1,63 @@
+from flask import jsonify, Blueprint
+from db.db_product_manager import DbProductManager
+from verify_input import general_data_validation, role_required
+from sqlalchemy.exc import SQLAlchemyError
+from api_exception import APIException
+
+product_bp = Blueprint("product", __name__)
+db_product_manager = DbProductManager()
+
+
+@product_bp.route("/products", methods=["POST"])
+@general_data_validation(["admin"], "name", "price", "amount")
+def register_product(id_user, name, price, amount):
+    try:
+        id_returned = db_product_manager.insert_product(name, price, amount)
+        return jsonify({"id": str(id_returned)}), 201
+    except SQLAlchemyError as e:
+        return jsonify(error=f"Internal database error: {e}"), 500
+    except APIException as e:
+        return jsonify(error=str(e)), e.status_code
+    except Exception as e:
+        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+
+
+@product_bp.route("/products", methods=["GET"])
+def get_products():
+    try:
+        results = db_product_manager.get_products()
+        return jsonify({"products": results}), 200
+    except SQLAlchemyError as e:
+        return jsonify(error=f"Internal database error: {e}"), 500
+    except APIException as e:
+        return jsonify(error=str(e)), e.status_code
+    except Exception as e:
+        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+
+
+@product_bp.route("/products/<product_id>", methods=["PUT"])
+@general_data_validation(["admin"], "name", "price", "amount")
+def update_product(id_user, product_id, name, price, amount):
+    try:
+        db_product_manager.update_product(int(product_id), name, price, amount)
+        return jsonify({"message": "Product Updated"}), 200
+    except SQLAlchemyError as e:
+        return jsonify(error=f"Internal database error: {e}"), 500
+    except APIException as e:
+        return jsonify(error=str(e)), e.status_code
+    except Exception as e:
+        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+
+
+@product_bp.route("/products/<product_id>", methods=["DELETE"])
+@role_required(["admin"])
+def delete_product(id_user,product_id):
+    try:
+        db_product_manager.delete_product(product_id)
+        return jsonify({"message": "Product Deleted"}), 200
+    except SQLAlchemyError as e:
+        return jsonify(error=f"Internal database error: {e}"), 500
+    except APIException as e:
+        return jsonify(error=str(e)), e.status_code
+    except Exception as e:
+        return jsonify(error=f"An unexpected error occurred: {e}"), 500
---
+++ M2/Back-end/S8/store/db/db_product_manager.py
@@ -0,0 +1,64 @@
+from sqlalchemy import insert, select, delete, update
+from db.tables_manager import TablesManager
+from api_exception import APIException
+
+product_table = TablesManager.product_table
+engine = TablesManager.engine
+
+
+class DbProductManager:
+
+    def insert_product(self, name: str, price: int, amount: int):
+        stmt = (
+            insert(product_table)
+            .returning(product_table.c.id)
+            .values(name=name, price=price, amount=amount)
+        )
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            conn.commit()
+            return result.scalar()
+
+    def get_products(self):
+        stmt = select(product_table)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            products = [dict(row) for row in result.mappings().all()]
+            return products
+
+    def get_product_by_id(self, id: int):
+        # D a select for id
+        stmt = select(product_table).where(product_table.c.id == id)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            products = result.all()
+            if len(products) == 0:
+                raise APIException(f"Product id:{str(id)} does not exist", 404)
+            else:
+                return products
+
+    def update_product(self, id: int, name: str, price: int, amount: int):
+        # Do a select for id
+        stmt = (
+            update(product_table)
+            .where(product_table.c.id == id)
+            .values(name=name, price=price, amount=amount)
+        )
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            rows_created = result.rowcount
+            if rows_created == 0:
+                raise APIException(f"Could not update product id:{str(id)}", 403)
+            else:
+                conn.commit()
+
+    def delete_product(self, id: int):
+        # Do a select for id
+        stmt = delete(product_table).where(product_table.c.id == id)
+        with engine.connect() as conn:
+            result = conn.execute(stmt)
+            rows_deleted = result.rowcount
+            if rows_deleted == 0:
+                raise APIException(f"Could not delete product id:{str(id)}", 403)
+            else:
+                conn.commit()
