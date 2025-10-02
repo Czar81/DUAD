5956874100130dce13550426f1891c8d2d80df51
Czar81 <ianaaronza@gmail.com>
2025-10-02 15:03:26 -0600

    Add new env and change the name of the imports

--- M2/Back-end/S8/store/encoding.py
+++ M2/Back-end/S8/store/encoding.py
@@ -1,18 +1,19 @@
 from jwt import encode, decode
 from os import environ
 from dotenv import load_dotenv
+
 load_dotenv()
+
 
 class JWT_Manager:
     def __init__(self):
-        self.private_key = os.environ.get("PRIVATE_KEY")
-        self.public_key = os.environ.get("PUBLIC_KEY")
+        self.private_key = environ.get("PRIVATE_KEY")
+        self.public_key = environ.get("PUBLIC_KEY")
 
     def encode(self, data):
-        encoded = jwt.encode(data, self.private_key, algorithm="RS256")
+        encoded = encode(data, self.private_key, algorithm="RS256")
         return encoded
 
     def decode(self, token):
-        decoded = jwt.decode(token, self.public_key, algorithms=["RS256"])
+        decoded = decode(token, self.public_key, algorithms=["RS256"])
         return decoded
-
