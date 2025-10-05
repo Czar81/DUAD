16eae2161b24ffbcd9e67317bff33d265091bcf8
Czar81 <ianaaronza@gmail.com>
2025-10-05 09:47:37 -0600

    Implemented cache with redis

--- M2/Back-end/S8/store/apis/product_api.py
+++ M2/Back-end/S8/store/apis/product_api.py
@@ -1,10 +1,12 @@
 from flask import jsonify, Blueprint
 from db.db_product_manager import DbProductManager
 from sqlalchemy.exc import SQLAlchemyError
-from utils import APIException, general_data_validation, role_required
+from redis import RedisError
+from utils import APIException, general_data_validation, role_required, CacheManager
 
 product_bp = Blueprint("product", __name__)
 db_product_manager = DbProductManager()
+cache_manager = CacheManager()
 
 
 @product_bp.route("/products", methods=["POST"])
@@ -12,6 +14,7 @@ db_product_manager = DbProductManager()
 def register_product(id_user, name, price, amount):
     try:
         id_returned = db_product_manager.insert_product(name, price, amount)
+        __delete_if_exist("getProducts-all")
         return jsonify({"id": str(id_returned)}), 201
     except SQLAlchemyError as e:
         return jsonify(error=f"Internal database error: {e}"), 500
@@ -23,13 +26,32 @@ def register_product(id_user, name, price, amount):
 
 @product_bp.route("/products", methods=["GET"])
 def get_products():
+    key = "getProducts-all"
     try:
-        results = db_product_manager.get_products()
-        return jsonify({"products": results}), 200
+        result = __get_cache_if_exist(key)
+        return jsonify({"products": result}), 200
     except SQLAlchemyError as e:
         return jsonify(error=f"Internal database error: {e}"), 500
     except APIException as e:
         return jsonify(error=str(e)), e.status_code
+    except RecursionError as e:
+        return jsonify(errror=f"An unexpected error occurred with redis: {e}"), 500
+    except Exception as e:
+        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+
+
+@product_bp.route("/products/<product_id>", methods=["GET"])
+def get_product_by_id(product_id):
+    key = f"getProduct:{product_id}"
+    try:
+        results = __get_cache_if_exist(key, product_id, by_id=True)
+        return jsonify({"product": result}), 200
+    except SQLAlchemyError as e:
+        return jsonify(error=f"Internal database error: {e}"), 500
+    except APIException as e:
+        return jsonify(error=str(e)), e.status_code
+    except RecursionError as e:
+        return jsonify(errror=f"An unexpected error occurred with redis: {e}"), 500
     except Exception as e:
         return jsonify(error=f"An unexpected error occurred: {e}"), 500
 
@@ -37,13 +59,17 @@ def get_products():
 @product_bp.route("/products/<product_id>", methods=["PUT"])
 @general_data_validation(["admin"], "name", "price", "amount")
 def update_product(id_user, product_id, name, price, amount):
+    key = f"getProduct:{product_id}"
     try:
         db_product_manager.update_product(int(product_id), name, price, amount)
+        __delete_if_exist(key)
         return jsonify({"message": "Product Updated"}), 200
     except SQLAlchemyError as e:
         return jsonify(error=f"Internal database error: {e}"), 500
     except APIException as e:
         return jsonify(error=str(e)), e.status_code
+    except RecursionError as e:
+        return jsonify(errror=f"An unexpected error occurred with redis: {e}"), 500
     except Exception as e:
         return jsonify(error=f"An unexpected error occurred: {e}"), 500
 
@@ -51,12 +77,41 @@ def update_product(id_user, product_id, name, price, amount):
 @product_bp.route("/products/<product_id>", methods=["DELETE"])
 @role_required(["admin"])
 def delete_product(id_user, product_id):
+    key = f"getProduct:{product_id}"
     try:
         db_product_manager.delete_product(product_id)
+        __delete_if_exist(key)
         return jsonify({"message": "Product Deleted"}), 200
     except SQLAlchemyError as e:
         return jsonify(error=f"Internal database error: {e}"), 500
     except APIException as e:
         return jsonify(error=str(e)), e.status_code
+    except RecursionError as e:
+        return jsonify(errror=f"An unexpected error occurred with redis: {e}"), 500
     except Exception as e:
         return jsonify(error=f"An unexpected error occurred: {e}"), 500
+
+
+def __delete_if_exist(key):
+    exist = cache_manager.check_key(key)
+    if exist:
+        cache_manager.delete_data(key)
+
+
+def __delete_if_exist(key):
+    exist = cache_manager.check_key(key)
+    if exist:
+        cache_manager.delete_data(key)
+
+
+def __get_cache_if_exist(key, id=None, by_id=False):
+    exist = cache_manager.check_key(key)
+    if exist:
+        result = cache_manager.get_data(key)
+    else:
+        if by_id and id is not None:
+            result = db_product_manager.get_product_by_id(id)
+        else:
+            result = db_product_manager.get_products()
+        cache_manager.store_data(key, result)
+    return result
