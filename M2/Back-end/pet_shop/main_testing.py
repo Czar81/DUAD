2c15651c0ab887638a986f4800a316ba706e1d11
Czar81 <ianaaronza@gmail.com>
2025-11-08 12:43:26 -0600

    Unless class

--- M2/Back-end/pet_shop/main_testing.py
+++
@@ -1,7 +0,0 @@
-from db.utils_db.tables_manager import TablesManager
-from testing.db_testing import DbTestingManager
-
-if __name__=="__main__":
-    TablesManager.create_tables()
-    db_testing=DbTestingManager()
-    db_testing.menu()
