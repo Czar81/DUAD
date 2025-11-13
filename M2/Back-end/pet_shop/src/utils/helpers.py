c6b0ca25cb9041f0c1972db00677b45ecbd7e39b
Czar81 <ianaaronza@gmail.com>
2025-11-12 20:11:50 -0600

    Add generate_cache_based_filters

--- M2/Back-end/pet_shop/src/utils/helpers.py
+++ M2/Back-end/pet_shop/src/utils/helpers.py
@@ -1,2 +1,10 @@
 def filter_values(locals_dict: dict, exclude: tuple = ("self",)):
     return {k: v for k, v in locals_dict.items() if k not in exclude and v is not None}
+
+def generate_cache_based_filters(key_prefix:str, filter_dict:dict):
+    value_filters=filter_values(locals_dict)
+    if value_filters:
+        cache_suffix = ":".join(f"{k}={v}" for k, v in sorted(value_filters.items()))
+        return f"{key_prefix}:{cache_suffix}"
+    else:
+        return f"{key_prefix}:all"
