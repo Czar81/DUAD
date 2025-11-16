35716d4374daafa0311da552b5dfd034b511921a
Czar81 <ianaaronza@gmail.com>
2025-11-16 08:52:00 -0600

    Implement handler in product

--- M2/Back-end/pet_shop/src/api/product_api.py
+++ M2/Back-end/pet_shop/src/api/product_api.py
@@ -7,9 +7,11 @@ from src.utils import (
     CacheManager,
     validate_fields,
     generate_cache_based_filters,
+    register_error_handlers,
 )
 
 product_bp = Blueprint("product", __name__)
+register_error_handlers(product_bp)
 db_product_manager = DbProductManager()
 cache_manager = CacheManager()
 
@@ -18,78 +20,44 @@ cache_manager = CacheManager()
 @role_required(["admin"])
 @validate_fields(required=["sku", "name", "price", "amount"])
 def register_product(sku, name, price, amount):
-    try:
-        id_product = db_product_manager.insert_data(sku, name, price, amount)
-        cache_manager.delete_data_with_pattern("getProducts:")
-        return jsonify({"id": f"Product created id{id_product}"}), 200
-    except ValueError as e:
-        return jsonify(error=str(e)), 400
-    except SQLAlchemyError as e:
-        return jsonify(error=f"Internal database error: {e}"), 500
-    except APIException as e:
-        return jsonify(error=str(e)), e.status_code
-    except RecursionError as e:
-        return jsonify(error=f"An unexpected error occurred with redis: {e}"), 500
-    except Exception as e:
-        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+    id_product = db_product_manager.insert_data(sku, name, price, amount)
+    cache_manager.delete_data_with_pattern("getProducts:")
+    return jsonify({"id": f"Product created id{id_product}"}), 200
 
 
 @product_bp.route("products", methods=["GET"])
 @validate_fields(optional=["id_product", "sku", "name", "price", "amount"])
 def get_products(**filters):
-    try:
-        key = generate_cache_based_filters("getProducts", filters)
-        result = __get_cache_if_exist(key, **filters)
-        return jsonify({"products": result}), 200
-    except ValueError as e:
-        return jsonify(error=str(e)), 400
-    except SQLAlchemyError as e:
-        return jsonify(error=f"Internal database error: {e}"), 500
-    except APIException as e:
-        return jsonify(error=str(e)), e.status_code
-    except RecursionError as e:
-        return jsonify(error=f"An unexpected error occurred with redis: {e}"), 500
-    except Exception as e:
-        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+    key = generate_cache_based_filters("getProducts", filters)
+    result = __get_cache_if_exist(key, **filters)
+    return jsonify({"products": result}), 200
 
 
 @product_bp.route("products/<id_product>", methods=["GET"])
 def get_product(id_product):
     try:
-        key = generate_cache_key("getProduct", id_product=id_product)
-        result = __get_cache_if_exist(key, id_product=int(id_product))
-        return jsonify({"product": result}), 200
-    except ValueError as e:
-        return jsonify(error=str(e)), 400
-    except SQLAlchemyError as e:
-        return jsonify(error=f"Internal database error: {e}"), 500
-    except APIException as e:
-        return jsonify(error=str(e)), e.status_code
-    except RecursionError as e:
-        return jsonify(error=f"An unexpected error occurred with redis: {e}"), 500
-    except Exception as e:
-        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+        id_product = int(id_product)
+    except ValueError:
+        return jsonify(error="id_product must be an integer"), 400
+    key = generate_cache_based_filters("getProduct", id_product=id_product)
+    result = __get_cache_if_exist(key, id_product=int(id_product))
+    return jsonify({"product": result}), 200
 
 
 @product_bp.route("products/<id_product>", methods=["PUT"])
 @role_required(["admin"])
 @validate_fields(optional=["sku", "name", "price", "amount"])
-def update_product(id_product,**filters):
+def update_product(id_product, **filters):
     try:
-        filters['id_product'] = id_product
-        key = generate_cache_key("getProduct", id_product=id_product)
-        db_product_manager.update_data(**filters)
-        cache_manager.delete_data(key)
-        cache_manager.delete_data_with_pattern("getProducts:")
-        return jsonify({"message": "Product Updated"}), 200
-    except SQLAlchemyError as e:
-        return jsonify(error=f"Internal database error: {e}"), 500
-    except APIException as e:
-        return jsonify(error=str(e)), e.status_code
-    except RecursionError as e:
-        return jsonify(error=f"An unexpected error occurred with redis: {e}"), 500
-    except Exception as e:
-        return jsonify(error=f"An unexpected error occurred: {e}"), 500
+        id_product = int(id_product)
+    except ValueError:
+        return jsonify(error="id_product must be an integer"), 400
+    filters["id_product"] = id_product
+    key = generate_cache_based_filters("getProduct", id_product=id_product)
+    db_product_manager.update_data(**filters)
+    cache_manager.delete_data(key)
+    cache_manager.delete_data_with_pattern("getProducts:")
+    return jsonify({"message": "Product Updated"}), 200
 
 
 @product_bp.route("products/<id_product>", methods=["DELETE"])
