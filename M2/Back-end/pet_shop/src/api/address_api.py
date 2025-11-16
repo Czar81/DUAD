8fb69ef5a999c4b381c55d297522c7a3177a70b4
Czar81 <ianaaronza@gmail.com>
2025-11-16 14:23:24 -0600

    --amead

--- M2/Back-end/pet_shop/src/api/address_api.py
+++ M2/Back-end/pet_shop/src/api/address_api.py
@@ -33,7 +33,7 @@ def get_address(id_user, role):
 
 @address_bp.route("<name>/address/<id_address>", methods=["GET"])
 @role_required(["admin","user"])
-def get_address(id_address, id_user):
+def get_address(id_address,role, id_user):
     if role == "user":
         address = db_address_manager.get_data(id_address,id_user)
     else:
