392752fae451ab597961473edb8b9be655a6877f
Czar81 <ianaaronza@gmail.com>
2025-11-16 14:24:38 -0600

    Create payment api

---
+++ M2/Back-end/pet_shop/src/api/payment_api.py
@@ -0,0 +1,52 @@
+from flask import jsonify, Blueprint
+from sqlalchemy.exc import SQLAlchemyError
+from src.db.user_module.db_payment_manager import DbPaymentManager
+from src.utils import (
+    APIException,
+    role_required,
+    validate_fields,
+    register_error_handlers,
+)
+
+payment_bp = Blueprint("payment", __name__)
+register_error_handlers(payment_bp)
+db_payment_manager = DbPaymentManager()
+
+
+@payment_bp.route("<name>/payment", methods=["POST"])
+@role_required(["admin","user"])
+@validate_fields(required=["type_data","data"])
+def register_payment(id_user, type_data, data):
+    db_payment_manager.insert_data(id_user, type_data, data)
+    return jsonify(message="Payement created"), 201
+
+
+@payment_bp.route("<name>/payment", methods=["GET"])
+@role_required(["admin","user"])
+@validate_fields(optional=["type_data"])
+def get_payment(id_user, role, type_data):
+    if role == "user":
+        payment = db_payment_manager.get_data(id_user=id_user, type=type_data)
+    else:
+        payment = db_payment_manager.get_data(type=type_data)
+    return jsonify(data=payment), 200
+
+
+@payment_bp.route("<name>/payment/<id_payment>", methods=["GET"])
+@role_required(["admin","user"])
+def get_payment(id_payment, role, id_user):
+    if role == "user":
+        payment = db_payment_manager.get_data(id_payment,id_user)
+    else:
+        payment = db_payment_manager.get_data(id_payment)
+    return jsonify(data=payment), 200
+
+
+@payment_bp.route("<name>/payment/<id_payment>", methods=["DELETE"])
+@role_required(["admin", "role"])
+def delete_payment(id_user, role, id_payment):
+    if role == "user":
+        db_payment_manager.delete_data(id_payment, id_user)
+    else:
+        db_payment_manager.delete_data(id_payment)
+    return jsonify(message="Payement deleted"), 200
