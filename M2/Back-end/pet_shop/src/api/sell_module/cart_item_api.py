164a047c8033d031b623cc85c5db649b31429257
Czar81 <ianaaronza@gmail.com>
2025-11-23 16:56:21 -0600

    Fix url

--- M2/Back-end/pet_shop/src/api/sell_module/cart_item_api.py
+++ M2/Back-end/pet_shop/src/api/sell_module/cart_item_api.py
@@ -11,22 +11,23 @@ register_error_handlers(cart_items_bp)
 db_cart_item_manager = DbCartItemsManager()
 
 
-@cart_items_bp.route("add-item", methods=["POST"])
+@cart_items_bp.route("/add-item", methods=["POST"])
 @role_required(["admin", "user"])
 @validate_fields(required=["id_cart","id_product", "amount"])
-def create_cart_item(id_cart,id_product, amount, id_user):
+def create_cart_item(id_cart,id_product, amount, id_user, role):
     db_cart_item_manager.insert_data(id_cart,id_product, amount, id_user)
     return jsonify(message="Item added"), 201
 
 
-@cart_items_bp.route("car/modify-item", methods=["PUT"])
+@cart_items_bp.route("/modify-item", methods=["PUT"])
+@validate_fields(required=["amount"])
 @role_required(["admin", "user"])
 def update_cart_item(id_user, amount, id_cart_item):
     db_cart_item_manager.update_data(id_cart_item, amount, id_user)
     return jsonify(message="Amounr updated"), 200
 
 
-@cart_items_bp.route("car/remove-item", methods=["DELETE"])
+@cart_items_bp.route("/remove-item", methods=["DELETE"])
 @role_required(["admin", "user"])
 def delete_cart_item(id_user, id_cart_item):
     db_cart_item_manager.delete_data(id_cart_item, id_user)
