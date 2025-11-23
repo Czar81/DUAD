7180e8502e3f6a0ae08e9f904bc6ee3b8323c733
Czar81 <ianaaronza@gmail.com>
2025-11-23 17:53:53 -0600

    Added main.py

---
+++ M2/Back-end/pet_shop/main.py
@@ -0,0 +1,30 @@
+from src.api.user_module.address_api import address_bp
+from src.api.user_module.payment_api import payment_bp
+from src.api.user_module.user_api import user_bp
+from src.api.sell_module.cart_api import cart_bp
+from src.api.sell_module.cart_item_api import cart_items_bp
+from src.api.sell_module.product_api import product_bp
+from src.api.sell_module.receipt_api import receipt_bp
+from flask import Flask
+from dotenv import load_dotenv
+from db.tables_manager import TablesManager
+from os import environ
+
+load_dotenv()
+
+app = Flask("Store-service")
+
+if __name__ == "__main__":
+    TablesManager.create_tables()
+    app.register_blueprint(product_bp)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(address_bp)
+    app.register_blueprint(payment_bp)
+    app.register_blueprint(cart_items_bp)
+    app.register_blueprint(cart_bp)
+    app.register_blueprint(receipt_bp)
+    app.run(
+        host=environ.get("HOST_API"),
+        port=environ.get("PORT_API"),
+        debug=environ.get("DEBUG_MODE"),
+    )
