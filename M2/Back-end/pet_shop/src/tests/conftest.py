31233bec1b2ebcf8c72be87dbdc34537881b1209
Czar81 <ianaaronza@gmail.com>
2025-11-24 20:33:56 -0600

    Added base db managers

--- M2/Back-end/pet_shop/src/tests/conftest.py
+++ M2/Back-end/pet_shop/src/tests/conftest.py
@@ -17,10 +17,85 @@ def db():
     tm.create_tables()
     return tm
 
+
 @pytest.fixture
 def base_user(db_user_manager):
-    user_id = db_user_manager.insert_data("TestUser", "12345", "user")
-    return user_id
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    return id_user
+
+
+@pytest.fixture
+def base_payment(db_user_manager, db_payment_manager):
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    id_payment = db_payment_manager.insert_data(id_user, "card", "data_payment")
+    return id_payment
+
+
+@pytest.fixture
+def base_address(db_user_manager, db_address_manager):
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    id_address = db_address_manager.insert_data(id_user, "9 Oak Valley Avenue")
+    return id_address
+
+
+@pytest.fixture
+def base_product(db_product_manager):
+    id_product = db_product_manager.insert_data(
+        "FD_NU_132", "nutrisource lite", 1000, 50
+    )
+    return id_product
+
+
+@pytest.fixture
+def base_cart(db_user_manager, db_cart_manager):
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    id_cart = db_cart_manager.insert_data(id_user)
+    return id_cart
+
+
+@pytest.fixture
+def base_cart_item(
+    request, db_cart_manager, db_user_manager, db_cart_item_manager, db_product_manager
+):
+    amount = request.param
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    id_cart = db_cart_manager.insert_data(id_user)
+    id_product = db_product_manager.insert_data(
+        "FD_NU_132", "nutrisource lite", 1000, 50
+    )
+    id_cart_item = db_cart_item_manager.insert_data(
+        id_cart, id_product, amount, id_user
+    )
+    return id_cart_item
+
+
+@pytest.fixture
+def base_receipt(
+    request,
+    db_user_manager,
+    db_cart_manager,
+    db_address_manager,
+    db_payment_manager,
+    db_cart_item_manager,
+    db_product_manager,
+    db_receipt_manager,
+):
+    amount, state = request.param
+    id_user = db_user_manager.insert_data("TestUser", "12345", "user")
+    id_payment = db_payment_manager.insert_data(id_user, "card", "data_payment")
+    id_address = db_address_manager.insert_data(id_user, "9 Oak Valley Avenue")
+    id_cart = db_cart_manager.insert_data(id_user)
+    id_product = db_product_manager.insert_data(
+        "FD_NU_132", "nutrisource lite", 1000, 50
+    )
+    id_cart_item = db_cart_item_manager.insert_data(
+        id_cart, id_product, amount, id_user
+    )
+    id_receip = db_receipt_manager.create_receipt(
+        id_cart, id_address, id_payment, state, id_user
+    )
+    return id_receip
+
 
 @pytest.fixture
 def db_payment_manager(db):
@@ -31,6 +106,7 @@ def db_payment_manager(db):
 def db_user_manager(db):
     return DbUserManager(db)
 
+
 @pytest.fixture
 def db_address_manager(db):
     return DbAddressManager(db)
@@ -40,13 +116,16 @@ def db_address_manager(db):
 def db_cart_item_manager(db):
     return DbCartItemsManager(db)
 
+
 @pytest.fixture
 def db_cart_manager(db):
     return DbCartManager(db)
 
+
 @pytest.fixture
 def db_product_manager(db):
     return DbProductManager(db)
+
 
 @pytest.fixture
 def db_receipt_manager(db):
