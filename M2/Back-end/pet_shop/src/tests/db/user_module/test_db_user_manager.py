d550df33474cebda87243d249011ec4991313c18
Czar81 <ianaaronza@gmail.com>
2025-11-24 18:25:43 -0600

    --amead

--- M2/Back-end/pet_shop/src/tests/db/user_module/test_db_user_manager.py
+++ M2/Back-end/pet_shop/src/tests/db/user_module/test_db_user_manager.py
@@ -3,7 +3,7 @@ import pytest
 def test_create_user(db_user_manager):
     username = "Ian"
     password = "2NFD9JDFI4324NJF20"
-    result_expected = 2
+    result_expected = 1
 
     id_user = db_user_manager.insert_data(username, password)
 
@@ -15,14 +15,14 @@ def test_create_admin(db_user_manager):
     password = "2NFD9JDsdaFI4324NJF20"
     role = "admin"
 
-    result_expected = 2
+    result_expected = 1
 
     id_user = db_user_manager.insert_data(username, password, role)
 
     assert id_user == result_expected
 
 
-def test_get_users(db_user_manager):
+def test_get_users(db_user_manager, base_user):
     result_expected = [
         {"id": 1, "username": "TestUser", "password": "12345", "role":"user"}
     ]
@@ -32,7 +32,9 @@ def test_get_users(db_user_manager):
     assert users == result_expected
 
 
-def test_login(db_user_manager):
+def test_login(db_user_manager, base_user):
+    username="TestUser"
+    password="12345"
     result_expected = 1
 
     me = db_user_manager.get_user(username, password)
@@ -71,6 +73,6 @@ def test_update_one_args_user(db_user_manager,base_user):
 def test_delete_user(db_user_manager, base_user):
     result_expected = True
 
-    result = db_user_manager.delete_data(id_user)
+    result = db_user_manager.delete_data(base_user)
 
     assert result == result_expected
