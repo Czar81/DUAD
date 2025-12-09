23e526cf3cedf94d462fb90bd8501d3ea44def60
Czar81 <ianaaronza@gmail.com>
2025-12-08 20:28:15 -0600

    Fix minor bugs

--- M2/Back-end/pet_shop/src/tests/db/sell_module/test_db_cart_manager.py
+++ M2/Back-end/pet_shop/src/tests/db/sell_module/test_db_cart_manager.py
@@ -10,30 +10,24 @@ def test_create_cart(db_cart_manager, base_user):
     assert result_expected == cart_created
 
 
-def test_create_cart_with_param_state(db_cart_manager, base_user):
-    id_user = base_user
-    state = "special"
-    result_expected = 1
-
-    cart_created = db_cart_manager.insert_data(id_user, state)
-
-    assert result_expected == cart_created
-
-
-def test_get_all_cart(db_cart_manager, base_cart):
+def test_get_all_cart(db_cart_manager, base_cart, ):
     id_cart = base_cart
     result_expected = [{"id": id_cart, "id_user": 1, "state": "active"}]
 
-    carts = db_cart_manager.get_data()
+    carts = db_cart_manager.get_data(id_user=1)
 
     assert result_expected == carts
 
+def test_get_active_cart(db_cart_manager, base_cart, ):
+   id_cart = base_cart
+   result_expected = [{"id": id_cart, "id_user": 1, "state": "active"}]
+   carts = db_cart_manager.get_data(id_user=1)
+   assert result_expected == carts
 
 def test_updates_cart_state(db_cart_manager, base_cart):
     id_cart = base_cart
     new_state = "active"
-
-    cart_created = db_cart_manager.insert_data(1, "inactive")
+    cart_created = db_cart_manager.insert_data(id_user=1)
     result_expected =    [
         {
             'id': id_cart,
@@ -47,8 +41,8 @@ def test_updates_cart_state(db_cart_manager, base_cart):
         },
     ]
 
-    updated = db_cart_manager.update_data(2, new_state, 1)
-    carts = db_cart_manager.get_data()
+    updated = db_cart_manager.update_data(id_cart=2, state=new_state, id_user=1)
+    carts = db_cart_manager.get_data(id_user=1)
 
     assert updated == True
     assert carts == result_expected
@@ -58,7 +52,7 @@ def test_delete_cart(db_cart_manager, base_cart):
     id_cart = base_cart
     result_expected = "Not carts found"
     deleted = db_cart_manager.delete_data(id_cart, 1)
-    carts = db_cart_manager.get_data()
+    carts = db_cart_manager.get_data(id_user=1)
 
     assert deleted == True
     assert carts == result_expected
--- M2/Back-end/pet_shop/src/tests/db/sell_module/test_db_receipt_manager.py
+++ M2/Back-end/pet_shop/src/tests/db/sell_module/test_db_receipt_manager.py
@@ -1,14 +1,19 @@
 import pytest
 from datetime import date
 
-def test_create_receipt(db_receipt_manager,db_address_manager,db_payment_manager, base_cart_item):
+
+def test_create_receipt(
+    db_receipt_manager, db_address_manager, db_payment_manager, base_cart_item
+):
     id_user = 1
     id_cart = 1
     id_address = db_address_manager.insert_data(id_user, "9 Oak Valley Avenue")
     id_payment = db_payment_manager.insert_data(id_user, "card", "data_payment")
     result_expected = 1
 
-    receipt_created = db_receipt_manager.create_receipt(id_cart, id_address, id_payment, id_user=id_user)
+    receipt_created = db_receipt_manager.create_receipt(
+        id_cart, id_address, id_payment, id_user=id_user
+    )
 
     assert result_expected == receipt_created
 
@@ -17,12 +22,12 @@ def test_get_all_receipt(db_receipt_manager, base_receipt):
     id_receipt = base_receipt
     result_expected = [
         {
-            'id': 1,
-            'id_address': 1,
-            'id_cart': 1,
-            'id_payment': 1,
-            'state': 'bought',
-            'entry_date': str(date.today()),
+            "id": 1,
+            "id_address": 1,
+            "id_cart": 1,
+            "id_payment": 1,
+            "state": "bought",
+            "entry_date": str(date.today()),
         }
     ]
     receipts = db_receipt_manager.get_data()
@@ -36,19 +41,47 @@ def test_update_receipt_state(db_receipt_manager, base_receipt):
     new_state = "special"
     result_expected = [
         {
-            'id': 1,
-            'id_address': 1,
-            'id_cart': 1,
-            'id_payment': 1,
-            'state': 'special',
-            'entry_date': str(date.today()),
+            "id": 1,
+            "id_address": 1,
+            "id_cart": 1,
+            "id_payment": 1,
+            "state": "special",
+            "entry_date": str(date.today()),
         }
     ]
-    receipts = db_receipt_manager.get_data()
-    print(receipts)
+
     updated = db_receipt_manager.update_data(id_receipt, new_state, id_user)
     receipts = db_receipt_manager.get_data()
 
     assert updated == True
     assert receipts == result_expected
 
+
+def test_return_receipt(db_receipt_manager, db_product_manager, base_receipt):
+    id_user = 1
+    id_receipt = base_receipt
+    result_receipt_expected = [
+        {
+            "id": 1,
+            "id_address": 1,
+            "id_cart": 1,
+            "id_payment": 1,
+            "state": "returned",
+            "entry_date": str(date.today()),
+        }
+    ]
+    result_product_expected = [
+        {
+            "id": 1,
+            "sku": "FD_NU_132",
+            "name": "nutrisource lite",
+            "price": 1000,
+            "amount": 50,
+        }
+    ]
+    returned = db_receipt_manager.return_receipt(id_user, id_receipt)
+    receipts = db_receipt_manager.get_data()
+    product = db_product_manager.get_data()
+    assert returned == True
+    assert receipts == result_receipt_expected
+    assert product == result_product_expected
