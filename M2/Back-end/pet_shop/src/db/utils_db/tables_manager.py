2b868a07213c4d24d3725c3eca1bfecd77329b95
Czar <ianaaronza@gmail.com>
2025-12-14 13:51:57 -0600

    Added on deleted and constraint

--- M2/Back-end/pet_shop/src/db/utils_db/tables_manager.py
+++ M2/Back-end/pet_shop/src/db/utils_db/tables_manager.py
@@ -1,6 +1,14 @@
 from sqlalchemy import MetaData
 from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy import (
+    Table,
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    CheckConstraint,
+    UniqueConstraint,
+)
 from dotenv import load_dotenv
 from datetime import date
 from os import environ
@@ -11,6 +19,7 @@ load_dotenv()
 class TablesManager:
     def __init__(self, url=environ.get("URL_POSTGRES")):
         self.engine = create_engine(url)
+
     metadata_obj = MetaData()
     product_table = Table(
         "product",
@@ -20,6 +29,8 @@ class TablesManager:
         Column("name", String(30), nullable=False),
         Column("price", Integer, nullable=False),
         Column("amount", Integer, nullable=False),
+        CheckConstraint("price > 0", name="chk_price_positive"),
+        CheckConstraint("amount >= 0", name="chk_amount_non_negative"),
     )
     user_table = Table(
         "user",
@@ -33,21 +44,21 @@ class TablesManager:
         "cart",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("id_user", Integer, ForeignKey("user.id")),
+        Column("id_user", Integer, ForeignKey("user.id", ondelete="CASCADE")),
         Column("state", String(10), server_default="active"),
     )
     address_table = Table(
         "address",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("id_user", Integer, ForeignKey("user.id")),
+        Column("id_user", Integer, ForeignKey("user.id", ondelete="CASCADE")),
         Column("location", String(30), nullable=False),
     )
     payment_table = Table(
         "payment",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("id_user", Integer, ForeignKey("user.id")),
+        Column("id_user", Integer, ForeignKey("user.id", ondelete="CASCADE")),
         Column("type_data", String(10), nullable=False),
         Column("data", String(30)),
     )
@@ -55,19 +66,21 @@ class TablesManager:
         "cart_item",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("id_cart", Integer, ForeignKey("cart.id")),
-        Column("id_product", Integer, ForeignKey("product.id")),
+        Column("id_cart", Integer, ForeignKey("cart.id", ondelete="CASCADE")),
+        Column("id_product", Integer, ForeignKey("product.id", ondelete="RESTRICT")),
         Column("amount", Integer, nullable=False),
+        CheckConstraint("amount > 0", name="chk_amount_plus_zero"),
+        UniqueConstraint("id_cart", "id_product", name="uk_cart_item_unique"),
     )
     receipt_table = Table(
         "receipt",
         metadata_obj,
         Column("id", Integer, primary_key=True),
-        Column("id_cart", Integer, ForeignKey("cart.id")),
-        Column("id_address", Integer, ForeignKey("address.id")),
-        Column("id_payment", Integer, ForeignKey("payment.id")),
+        Column("id_cart", Integer, ForeignKey("cart.id", ondelete="RESTRICT")),
+        Column("id_address", Integer, ForeignKey("address.id", ondelete="RESTRICT")),
+        Column("id_payment", Integer, ForeignKey("payment.id", ondelete="RESTRICT")),
         Column("entry_date", String(10), server_default=str(date.today())),
-        Column("state", String(10), server_default="bought"),
+        Column("state", String(10), server_default="paid"),
     )
 
     def create_tables(self):
