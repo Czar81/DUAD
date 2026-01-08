a276a0fd53a74875bc1c929cebcdc8ea76642b48
Czar <ianaaronza@gmail.com>
2026-01-08 09:50:54 -0600

    Fix function

--- M2/Front-end/JS/async_await/get_user.js
+++ M2/Front-end/JS/async_await/get_user.js
@@ -3,13 +3,9 @@ async function getUser(userId) {
   const url = `https://jsonplaceholder.typicode.com/users/${userId}`;
   try {
     const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error ${response.status}`);
-    }
-
     const data = await response.json();
     console.log(data);
+    
   } catch (err) {
     console.log(`Error happened ${err}`);
   }
