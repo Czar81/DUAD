2e17c79c70542896a8171571e0df3a4a9aa863d1
Czar <ianaaronza@gmail.com>
2026-01-10 23:08:26 -0600

    Formatted files

--- M2/Front-end/JS/requests/login/script.js
+++ M2/Front-end/JS/requests/login/script.js
@@ -1,52 +1,48 @@
-
 async function getUser(userID) {
-    const url = `https://api.restful-api.dev/objects/${userID}`;
-    const response = await axios.get(url);
-    return response.data
+  const url = `https://api.restful-api.dev/objects/${userID}`;
+  const response = await axios.get(url);
+  return response.data;
 }
 
-
 function validatePass(pass1, pass2) {
-    console.log(pass1, pass2)
-    if (pass1 === pass2) {
-        errorMsg.textContent = "";
-        return true;
-    } else {
-        errorMsg.textContent = "El ID o la contraseña son incorrectas";
-        return false;
-    }
+  console.log(pass1, pass2);
+  if (pass1 === pass2) {
+    errorMsg.textContent = "";
+    return true;
+  } else {
+    errorMsg.textContent = "El ID o la contraseña son incorrectas";
+    return false;
+  }
 }
 
 async function main() {
-    try {
-        btn_submit.disabled = true;
-        const userID = document.getElementById("user-id").value
-        const user = await getUser(userID);
-        if(!validatePass(passInput.value, user.data.password)){
-            return;
-        }
-        document.cookie = `userID=${user.id}; path=/; SameSite=Lax; max-age=31536000`
-        alert(`Sesion iniciada correctamente, tu id es: ${String(user.id)}`);
-        setTimeout(() => {
-            location.replace("../profile/my_profile.html");
-        }, 50);
-    } catch (err) {
-        console.error(err);
-        errorMsg.textContent = "Error al iniciar sesion";
-    } finally {
-        btn_submit.disabled = false;
+  try {
+    btn_submit.disabled = true;
+    const userID = document.getElementById("user-id").value;
+    const user = await getUser(userID);
+    if (!validatePass(passInput.value, user.data.password)) {
+      return;
     }
+    document.cookie = `userID=${user.id}; path=/; SameSite=Lax; max-age=31536000`;
+    alert(`Sesion iniciada correctamente, tu id es: ${String(user.id)}`);
+    setTimeout(() => {
+      location.replace("../profile/my_profile.html");
+    }, 10);
+  } catch (err) {
+    console.error(err);
+    errorMsg.textContent = "Error al iniciar sesion";
+  } finally {
+    btn_submit.disabled = false;
+  }
 }
 
-const form = document.getElementById("form-login")
-const passInput = document.getElementById("password-user")
+const form = document.getElementById("form-login");
+const passInput = document.getElementById("password-user");
 
 const errorMsg = document.getElementById("errorMsg");
-const btn_submit = document.getElementById("btn-login")
-
-
+const btn_submit = document.getElementById("btn-login");
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    main()
-})
+  event.preventDefault();
+  main();
+});
--- M2/Front-end/JS/requests/register/script.js
+++ M2/Front-end/JS/requests/register/script.js
@@ -1,64 +1,63 @@
 async function createUser(data) {
-    const url = "https://api.restful-api.dev/objects";
-    const response = await axios.post(url, data);
-    return response.data
+  const url = "https://api.restful-api.dev/objects";
+  const response = await axios.post(url, data);
+  return response.data;
 }
 
 function formatValues() {
-    return {
-        name: document.getElementById("name-user").value,
-        data: {
-            email: document.getElementById("email-user").value,
-            address: document.getElementById("short-address").value,
-            password: document.getElementById("password-user").value
-        }
-    };
+  return {
+    name: document.getElementById("name-user").value,
+    data: {
+      email: document.getElementById("email-user").value,
+      address: document.getElementById("short-address").value,
+      password: document.getElementById("password-user").value,
+    },
+  };
 }
 
 function validatePass(pass1, pass2) {
-    if (pass1 === pass2) {
-        errorMsg.textContent = "";
-        return true;
-    } else {
-        errorMsg.textContent = "Las contraseñas no coinciden";
-        return false;
-    }
+  if (pass1 === pass2) {
+    errorMsg.textContent = "";
+    return true;
+  } else {
+    errorMsg.textContent = "Las contraseñas no coinciden";
+    return false;
+  }
 }
 
 async function main() {
-    if (!validatePass(pass1Input.value, pass2Input.value)) {
-        return;
-    }
-    try {
-        btn_submit.disabled = true;
-        const values = formatValues();
-        const response = await createUser(values);
-        document.cookie = `userID=${response.id}; path=/; SameSite=Lax; max-age=31536000`
-        alert(`Usuario creado correctamente, tu id es: ${String(response.id)}`);
-        setTimeout(() => {
-    location.replace("../profile/my_profile.html");
-}, 50);
-    } catch (err) {
-        console.error(err);
-        errorMsg.textContent = "Error al crear el usuario";
-    } finally {
-        btn_submit.disabled = false;
-    }
+  if (!validatePass(pass1Input.value, pass2Input.value)) {
+    return;
+  }
+  try {
+    btn_submit.disabled = true;
+    const values = formatValues();
+    const response = await createUser(values);
+    document.cookie = `userID=${response.id}; path=/; SameSite=Lax; max-age=31536000`;
+    alert(`Usuario creado correctamente, tu id es: ${String(response.id)}`);
+    setTimeout(() => {
+      location.replace("../profile/my_profile.html");
+    }, 50);
+  } catch (err) {
+    console.error(err);
+    errorMsg.textContent = "Error al crear el usuario";
+  } finally {
+    btn_submit.disabled = false;
+  }
 }
 
-const form = document.getElementById("form-register")
-const pass2Input = document.getElementById("password-confirm")
-const pass1Input = document.getElementById("password-user")
+const form = document.getElementById("form-register");
+const pass2Input = document.getElementById("password-confirm");
+const pass1Input = document.getElementById("password-user");
 
 const errorMsg = document.getElementById("errorMsg");
-const btn_submit = document.getElementById("btn-register")
-
+const btn_submit = document.getElementById("btn-register");
 
 pass2Input.addEventListener("input", () => {
-    validatePass(pass1Input.value, pass2Input.value)
+  validatePass(pass1Input.value, pass2Input.value);
 });
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    main()
-})
+  event.preventDefault();
+  main();
+});
