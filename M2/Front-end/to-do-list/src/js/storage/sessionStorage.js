4978ecb91be0952486b5b5bc2288844c8a4f3387
Czar <ianaaronza@gmail.com>
2026-01-17 15:39:50 -0600

    Add max age for userID

--- M2/Front-end/to-do-list/src/js/storage/sessionStorage.js
+++ M2/Front-end/to-do-list/src/js/storage/sessionStorage.js
@@ -2,6 +2,6 @@ import { setCookie, getCookie, deleteCookie } from "../utils/cookie.js";
 
 const UID_KEY = "uid";
 
-export const setUserId = (id) => setCookie(UID_KEY, id);
+export const setUserId = (id) => setCookie(UID_KEY, id, "max-age=31536000");
 export const getUserId = () => getCookie(UID_KEY);
 export const clearUserId = () => deleteCookie(UID_KEY);
