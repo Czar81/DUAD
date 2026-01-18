0f91b2a60266f37a91d6dc00431eb9e8c3733060
Czar <ianaaronza@gmail.com>
2026-01-18 20:47:16 +0000

    Fix bug on index

--- M2/Front-end/to-do-list/src/js/pages/index.js
+++ M2/Front-end/to-do-list/src/js/pages/index.js
@@ -1,14 +1,14 @@
 import { checkSession } from "../services/authService.js"
 import { getCookie } from "../utils/cookie.js"
 
-export const initIndexPage = () => {
+export const initIndexPage = async () => {
     const uid = getCookie("uid")
     if (!uid) {
-        window.location.replace("/M2/Front-end/to-do-list/src/pages/signup.html")
+        return window.location.replace("/M2/Front-end/to-do-list/src/pages/signup.html")
     }
-    const valid = checkSession(uid)
+    const valid = await checkSession(uid)
     if (!valid) {
-        window.location.replace("/M2/Front-end/to-do-list/src/pages/login.html")
-    }   
+        return window.location.replace("/M2/Front-end/to-do-list/src/pages/login.html")
+    }
     window.location.replace("/M2/Front-end/to-do-list/src/pages/to-do.html")
 }
