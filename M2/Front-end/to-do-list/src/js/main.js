ebbc08b7378924b12c3998c436fea9839e917855
Czar <ianaaronza@gmail.com>
2026-01-19 10:42:04 -0600

    Add global try catch

--- M2/Front-end/to-do-list/src/js/main.js
+++ M2/Front-end/to-do-list/src/js/main.js
@@ -4,29 +4,33 @@ import { initToDoListPage } from "./pages/to_do.js";
 import { initAboutPage } from "./pages/about.js";
 import { initIndexPage } from "./pages/index.js";
 
-const page = document.body.dataset.page;
+try {
+  const page = document.body.dataset.page;
 
-switch (page) {
-  case "login":
-    initLoginPage();
-    break;
+  switch (page) {
+    case "login":
+      initLoginPage();
+      break;
 
-  case "sign-up":
-    initSignupPage();
-    break;
+    case "sign-up":
+      initSignupPage();
+      break;
 
-  case "to-do":
-    initToDoListPage();
-    break;
+    case "to-do":
+      initToDoListPage();
+      break;
 
-  case "about":
-    initAboutPage();
-    break;
+    case "about":
+      initAboutPage();
+      break;
 
-  case "index":
-    initIndexPage();
-    break;
+    case "index":
+      initIndexPage();
+      break;
 
-  default:
-    console.warn("Unknown page");
+    default:
+      console.warn("Unknown page");
+  }
+} catch (err) {
+    console.error(err)
 }
