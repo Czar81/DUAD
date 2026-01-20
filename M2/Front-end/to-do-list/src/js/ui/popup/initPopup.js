100bc6be95e1504a1ad8f514bfd35b4d5ab9ca51
Czar <ianaaronza@gmail.com>
2026-01-20 15:14:21 -0600

    Popups

---
+++ M2/Front-end/to-do-list/src/js/ui/popup/initPopup.js
@@ -0,0 +1,25 @@
+import { popup } from "./popup.js";
+import {showPopupProfile} from "./profilePopup.js"
+
+export const openPopup = ({ type, message }) => {
+  switch (type) {
+    case "error":
+      popup("popup-error", "Error", "/src/assets/icons/error.svg", message);
+      break;
+    case "info":
+      popup("popup-info", "Info", "/src/assets/icons/info.svg", message);
+      break;
+    case "warn":
+      popup("popup-warn", "Warn", "/src/assets/icons/warn.svg", message);
+      break;
+    case "success":
+      popup("popup-success", "Success", "/src/assets/icons/success.svg", message,);
+      break;
+    case profile:
+      showPopupProfile()
+      break;
+    default:
+      console.warn("Unknow popup type");
+      break;
+  }
+};
---
+++ M2/Front-end/to-do-list/src/js/ui/popup/popup.js
@@ -0,0 +1,28 @@
+export const popup = (typeInfo ,iconSrc, iconAlt, message) => {
+const popup = document.createElement("div");
+  popup.className = `popup ${typeInfo} visible`;
+  const icon = document.createElement("img");
+  icon.className = "popup-icon";
+  icon.src = iconSrc;
+  icon.alt = iconAlt;
+
+  const title = document.createElement("p");
+  title.className = "popup-title";
+  title.textContent = message;
+
+  const close = document.createElement("img");
+  close.className = "popup-close";
+  close.src = "/src/assets/icons/close.svg";
+  close.alt="Close"
+
+  close.addEventListener("click", () => {
+    popup.remove();
+  });
+
+  popup.append(icon, title, close);
+  document.body.appendChild(popup);
+
+  setTimeout(() => {
+    popup.remove();
+  }, 3000);
+}
